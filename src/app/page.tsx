12283e0b06553b6eb31d690c2e7a4603891fd246
kendjr <ken@hamlethub.com>
2025-08-18 15:29:17 -0400

    updated front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          src="/images/pbailogo.svg"
+          alt="Palm Beaches AI Logo"
+          width={300}
+          height={300}
+          className="mb-8"
+        />
         <h1 className="text-4xl font-bold text-center">
           The Palm Beaches AI website will be coming soon!
         </h1>
